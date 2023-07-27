15d3a625c8fd150615657493d88bcfc3bc7e1906
canhlinh <canhlinh.lienson@gmail.com>
2023-07-27 15:54:46 +0700

    New gas and fee logic

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.2",
+  solidity: "0.8.20",
+  settings: {
+    viaIR: true,
+    optimizer: {
+      enabled: true,
+      details: {
+        yulDetails: {
+          optimizerSteps: "u",
+        },
+      },
+    },
+  },
 };
